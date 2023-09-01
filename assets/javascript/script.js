fb9717a3a2430fb93eeb91843de9ec33d51a0a67
dscornaienchi <Dscorni32@gmail.com>
2023-08-31 21:07:04 -0500

    Finalized styling of webpage and added if statement to js logic

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -11,11 +11,15 @@ resetCityListButton.addEventListener('click',resetCityList);
 function saveCity(event) {
     event.preventDefault();
     const city = cityInput.value.trim();
-    const newCity = { city };
+    if (city === "") {
+        alert('Please enter a city name');
+    } else {
+        const newCity = { city };
 
-    cityList.push(newCity);
-    localStorage.setItem('cityList', JSON.stringify(cityList));
-    showCities();
+        cityList.push(newCity);
+        localStorage.setItem('cityList', JSON.stringify(cityList));
+        showCities();
+    }
 }
 
 function showCities() {
@@ -23,6 +27,9 @@ function showCities() {
     cityList.forEach(function(cityObj) {
     var li = document.createElement('li');
     li.textContent = cityObj.city;
+
+    li.classList.add('list-group-item', 'list-group-item-dark', 'text-center', 'mb-2', 'rounded');
+
     cityListElement.appendChild(li);
     });
 }
