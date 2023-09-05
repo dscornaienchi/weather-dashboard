28d8df2ff672d5bc01bd7879b17fe48831e6dc84
dscornaienchi <Dscorni32@gmail.com>
2023-09-05 18:30:08 -0500

    completed javascript for city weather shell

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,23 +1,62 @@
 
 $(function () {
-//var APIKey = "e8dad390ebbd6c69a9686f2a12eedb94";
-//var city;
-//var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-//var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+var APIKey = "e8dad390ebbd6c69a9686f2a12eedb94";
 var cityInput = document.getElementById('city-input');
+var cityWeather = document.getElementById('city-weather');
 var cityListElement = document.getElementById('city-list');
 var saveCityButton = document.getElementById('save-city-button');
 var resetCityListButton = document.getElementById('reset-city-list-button');
-//var currentDate = dayjs().format('MM/DD/YYYY') $('#currentDay').text(currentDate);
+var cityList = JSON.parse(localStorage.getItem('cityList')) || [];
+//var lat;
+//var lon;
+//var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
 
-let cityList = JSON.parse(localStorage.getItem('cityList')) || [];
-
-saveCityButton.addEventListener('click', saveCity);
 resetCityListButton.addEventListener('click',resetCityList);
 
-//fetch(queryURL)
+function updateCityWeather(cityName, weatherData, tempKelvin, wind, humidity) {
+    var tempFahrenheit = Math.round((tempKelvin - 273.15) * 9/5 + 32);
+    var weatherIconCode = weatherData[0].icon;
+    var iconURL = `http://openweathermap.org/img/w/${weatherIconCode}.png`;
+    cityWeather.innerHTML = `
+    <h2>${cityName} ${dayjs().format('M/DD/YYYY')}</h2>
+    <img src="${iconURL}" alt="Weather Icon">
+    <h4>Temp: ${tempFahrenheit}°F</h4>
+    <h4>Wind: ${wind} MPH</h4>
+    <h4>Humidity: ${humidity}%</h4>
+    `;
+}
 
-//fetch(forecastURL)
+$('#save-city-button').on('click',function(event) {
+    event.preventDefault();
+    var city = cityInput.value.trim();
+
+    if (city === "") {
+        alert('Please enter a city name');
+    } else {
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+        fetch(queryURL)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                var cityName = data.name;
+                var temp = data.main.temp;
+                var wind = data.wind.speed;
+                var humidity = data.main.humidity;
+
+                updateCityWeather(cityName, data.weather, temp, wind, humidity);
+            })
+            .catch(function (error) {
+                console.log(error);
+                alert('Error fetching weather data. Please try again.');
+            })
+
+        cityList.push({city: city});
+        localStorage.setItem('cityList', JSON.stringify(cityList));
+        showCities();
+    }
+});
 
 function saveCity(event) {
     event.preventDefault();
@@ -49,6 +88,12 @@ function resetCityList(event) {
     event.preventDefault();
     localStorage.removeItem('cityList');
     cityList = [];
+    cityWeather.innerHTML = `
+        <h2>Please enter a city name</h2>
+        <h4>Temp: </h4>
+        <h4>Wind: </h4>
+        <h4>Humidity: </h4>
+        `;
     showCities();
 }
 
